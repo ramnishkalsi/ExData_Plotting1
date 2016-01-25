c6a775fac3d605265f631763d662f7b96cbce79e
Ramnish Kalsi <Ramnish.Kalsi@gmail.com>
2016-01-25 16:55:38 +0100

    Week 1 - Plot 1
    Plot 1 - R file & the png.

---
+++ Week1Plot1.R
@@ -0,0 +1,17 @@
+a1 <- function() {
+  library(dplyr)
+  LOCAL_FILE = "household_power_consumption.txt"  
+  
+  classes <- c("Date", "character", "numeric", "numeric","numeric","numeric","numeric","numeric","numeric")
+  
+  data1 <- read.table(LOCAL_FILE, header=TRUE, sep=";", colClasses=classes, na.strings=c("?") )
+    
+  Day1 <- filter(data1, Date=="1/2/2007")
+  Day2 <- filter(data1, Date=="2/2/2007")
+  
+  DayUnion <- rbind(Day1, Day2)
+  
+  hist(DayUnion$Global_active_power[ !DayUnion$Global_active_power=='?'], 
+       col = "red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+  
+}
